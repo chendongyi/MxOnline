6c24e258c80a0aa63a21d96fcbcf291d4c2fefb9
Chen Dongyi <cdy19861111@163.com>
2018-07-20 12:45:27 +0800

    bug fix: 解决DjangoUeditor不支持python3的问题

--- extra_apps/DjangoUeditor/urls.py
+++ extra_apps/DjangoUeditor/urls.py
@@ -1,21 +1,22 @@
-#coding:utf-8
-from django import VERSION
-from .widgets import UEditorWidget, AdminUEditorWidget
+# -*- coding: utf-8 -*-
+import django
 from .views import get_ueditor_controller
 
-if VERSION[0:2]<(2,0):
-    if VERSION[0:2]>(1,3):
+DJANGO_VERSION = django.VERSION[:2]
+
+
+if DJANGO_VERSION >= (1, 8):
+    from django.conf.urls import url
+    urlpatterns = [
+        url(r'^controller/$', get_ueditor_controller)
+    ]
+
+else:
+    try:
         from django.conf.urls import patterns, url
-    else:
+    except ImportError:
         from django.conf.urls.defaults import patterns, url
+
     urlpatterns = patterns('',
         url(r'^controller/$', get_ueditor_controller)
     )
-else :
-    from django.urls import path
-    urlpatterns = [
-        path('controller/', get_ueditor_controller),
-    ]
-
-
-
