ba40e4b563b8bf3f508d55efe80cb79680d9672d
Chen Dongyi <cdy19861111@163.com>
2018-07-20 10:58:09 +0800

    bug fix: 解决DjangoUeditor扩展包里不支持django2.0的问题

--- extra_apps/DjangoUeditor/urls.py
+++ extra_apps/DjangoUeditor/urls.py
@@ -3,11 +3,19 @@ from django import VERSION
 from .widgets import UEditorWidget, AdminUEditorWidget
 from .views import get_ueditor_controller
 
-if VERSION[0:2]>(1,3):
-    from django.conf.urls import patterns, url
-else:
-    from django.conf.urls.defaults import patterns, url
+if VERSION[0:2]<(2,0):
+    if VERSION[0:2]>(1,3):
+        from django.conf.urls import patterns, url
+    else:
+        from django.conf.urls.defaults import patterns, url
+    urlpatterns = patterns('',
+        url(r'^controller/$', get_ueditor_controller)
+    )
+else :
+    from django.urls import path
+    urlpatterns = [
+        path('controller/', get_ueditor_controller),
+    ]
 
-urlpatterns = patterns('',
-    url(r'^controller/$',get_ueditor_controller)
-)
+
+
